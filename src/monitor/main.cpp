c768740bcd05e4449d29cb8152394f1b7f993974
Michał Walenciak <Kicer86@gmail.com>
2020-10-10 10:07:04 +0200

    simple hello world

--- src/monitor/main.cpp
+++ src/monitor/main.cpp
@@ -1,9 +1,13 @@
 
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+
+    QLabel l("monitor");
+    l.show();
 
     return app.exec();
 }
