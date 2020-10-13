24780c64f00416c5147c513a23988dfa390776b5
Michał Walenciak <Kicer86@gmail.com>
2020-10-13 21:33:50 +0200

    cleanup with missing/not needed headers, code format

--- src/agent/SmartReaderFactory.h
+++ src/agent/SmartReaderFactory.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include <memory>
 #include "ISmartReader.h"
 
 class SmartReaderFactory
 {
 public:
-
 	std::unique_ptr<ISmartReader> GetSMARTReader();
 };
--- src/agent/main.cpp
+++ src/agent/main.cpp
@@ -1,16 +1,12 @@
+
 #include <iostream>
 
-#include <memory>
 #include "HealthChecker.h"
-
-
-
 
 int main(void)
 {
-
 	HealthChecker checker;
-	
+
 	std::string health = checker.CheckDiscHealth();
 
 	std::cout << health;
