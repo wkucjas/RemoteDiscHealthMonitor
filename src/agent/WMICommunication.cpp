0554f0c35917caeceda16cac5036d292c8eb426c
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-10-14 19:55:14 +0200

    Added WMICommunication class.

---
+++ src/agent/WMICommunication.cpp
@@ -0,0 +1,6 @@
+#include "WMICommunication.h"
+
+bool WMICommunication::WMIInit()
+{
+	return false;
+}
---
+++ src/agent/WMICommunication.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class WMICommunication
+{
+public:
+
+private:
+	bool WMIInit();
+
+
+};
