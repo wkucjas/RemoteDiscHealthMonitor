7200a205cc19e7f2cc61c399b96f1901eea5251e
Michał Walenciak <Kicer86@gmail.com>
2020-10-16 12:55:15 +0200

    publish yourself via zeroconf

--- src/agent/main.cpp
+++ src/agent/main.cpp
@@ -1,17 +1,28 @@
 
 #include <iostream>
+#include <QCoreApplication>
 
+#include "QtZeroConf/qzeroconf.h"
 #include "HealthChecker.h"
 #include "SmartReader.h"
+#include "common/constants.hpp"
 
-int main(void)
+int main(int argc, char** argv)
 {
+    QCoreApplication app(argc, argv);
+
+    QZeroConf zeroConf;
+    zeroConf.addServiceTxtRecord("RDHAgent");
+    zeroConf.startServicePublish("RDHAgent", ZeroConfServiceName.toStdString().c_str(), "", RDHMPort);
+
     SmartReader smartReader;
 	HealthChecker checker(smartReader);
 
 	std::string health = checker.CheckDiscHealth();
 
 	std::cout << health;
+
+    app.exec();
 
 	return 0;
 }
