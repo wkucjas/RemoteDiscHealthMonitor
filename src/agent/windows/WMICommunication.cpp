6de984350fb668f367e59e13b585ce8d9ca33a12
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-10-18 16:30:08 +0200

    Wrapped WMIInit by try/catch.

--- src/agent/windows/WMICommunication.cpp
+++ src/agent/windows/WMICommunication.cpp
@@ -5,7 +5,7 @@
 #define _WIN32_DCOM
 #include <iostream>
 #include <comdef.h>
-#pragma comment(lib, "wbemuuid.lib")
+//#pragma comment(lib, "wbemuuid.lib")
 
 
 WMICommunication::~WMICommunication()
@@ -18,92 +18,91 @@ WMICommunication::~WMICommunication()
 
 bool WMICommunication::WMIInit()
 {
-    HRESULT hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    try {
+        HRESULT hres = CoInitializeEx(0, COINIT_MULTITHREADED);
 
-    if (FAILED(hres))
-    {
-        throw std::exception("Failed to initialize COM library. Error code = 0x");
-        return false;
+        if (FAILED(hres))
+        {
+            throw std::exception("Failed to initialize COM library. Error code = 0x");
+        }
+
+        hres = CoInitializeSecurity(
+            NULL,
+            -1,                          // COM authentication
+            NULL,                        // Authentication services
+            NULL,                        // Reserved
+            RPC_C_AUTHN_LEVEL_PKT,       // Default authentication 
+            RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+            NULL,                        // Authentication info
+            EOAC_NONE,                   // Additional capabilities 
+            NULL                         // Reserved
+        );
+
+        if (FAILED(hres))
+        {
+            throw std::exception("Failed to initialize security. Error code = 0x");
+        }
+
+        hres = CoCreateInstance(
+            CLSID_WbemLocator,
+            0,
+            CLSCTX_INPROC_SERVER,
+            IID_IWbemLocator, (LPVOID*)&initialLocatorToWMI);
+
+        if (FAILED(hres))
+        {
+            throw std::exception("Failed to create IWbemLocator object. Err code = 0x");
+        }
+
+        hres = initialLocatorToWMI->ConnectServer(
+            _bstr_t(L"ROOT\\WMI"),   // Object path of WMI namespace
+            NULL,                    // User name. NULL = current user
+            NULL,                    // User password. NULL = current
+            0,                       // Locale. NULL indicates current
+            NULL,                    // Security flags.
+            0,                       // Authority (for example, Kerberos)
+            0,                       // Context object 
+            &services                // pointer to IWbemServices proxy
+        );
+
+        if (FAILED(hres))
+        {
+            throw std::exception("Could not connect. Error code = 0x");
+        }
+
+        hres = CoSetProxyBlanket(
+            services,                    // Indicates the proxy to set
+            RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+            RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+            NULL,                        // Server principal name 
+            RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+            RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+            NULL,                        // client identity
+            EOAC_NONE                    // proxy capabilities 
+        );
+
+        if (FAILED(hres))
+        {
+            throw std::exception("Could not set proxy blanket. Error code = 0x");
+        }
+
+        return true;
     }
-
-    hres = CoInitializeSecurity(
-        NULL,
-        -1,                          // COM authentication
-        NULL,                        // Authentication services
-        NULL,                        // Reserved
-        RPC_C_AUTHN_LEVEL_PKT,       // Default authentication 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
-        NULL,                        // Authentication info
-        EOAC_NONE,                   // Additional capabilities 
-        NULL                         // Reserved
-    );
-
-
-    if (FAILED(hres))
+    catch (...)
     {
         CoUninitialize();
-        throw std::exception("Failed to initialize security. Error code = 0x");
-        return false;
-    }
+        if (initialLocatorToWMI != NULL)
+        {
+            initialLocatorToWMI->Release();
+        }
 
-
-
-    hres = CoCreateInstance(
-        CLSID_WbemLocator,
-        0,
-        CLSCTX_INPROC_SERVER,
-        IID_IWbemLocator, (LPVOID*)&initialLocatorToWMI);
-
-    if (FAILED(hres))
-    {
-        CoUninitialize();
-        throw std::exception("Failed to create IWbemLocator object. Err code = 0x");
+        if (services != NULL)
+        {
+            services->Release();
+        }
 
         return false;
     }
-
-    hres = initialLocatorToWMI->ConnectServer(
-        _bstr_t(L"ROOT\\WMI"),   // Object path of WMI namespace
-        NULL,                    // User name. NULL = current user
-        NULL,                    // User password. NULL = current
-        0,                       // Locale. NULL indicates current
-        NULL,                    // Security flags.
-        0,                       // Authority (for example, Kerberos)
-        0,                       // Context object 
-        &services                // pointer to IWbemServices proxy
-    );
-
-    if (FAILED(hres))
-    {
-        initialLocatorToWMI->Release();
-        CoUninitialize();
-        throw std::exception("Could not connect. Error code = 0x");
-        return false;
-    }
-
-
-    hres = CoSetProxyBlanket(
-        services,                    // Indicates the proxy to set
-        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
-        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
-        NULL,                        // Server principal name 
-        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-        NULL,                        // client identity
-        EOAC_NONE                    // proxy capabilities 
-    );
-
-    if (FAILED(hres))
-    {
-
-        services->Release();
-        initialLocatorToWMI->Release();
-        CoUninitialize();
-        throw std::exception("Could not set proxy blanket. Error code = 0x");
-        return false;
-    }
-
-	return true;
 }
 
 bool WMICommunication::GetSMARTDataViaWMI()
@@ -119,7 +118,7 @@ bool WMICommunication::GetSMARTDataViaWMI()
     if (FAILED(hres))
     {
         services->Release();
-        services->Release();
+        initialLocatorToWMI->Release();
         CoUninitialize();
         throw std::exception("Query for operating system name failed. Error code = 0x");
         return false;
