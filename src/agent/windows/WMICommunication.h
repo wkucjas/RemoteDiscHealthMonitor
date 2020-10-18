573acdfcb4445472be37df8213b68e201465c2bf
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-10-18 18:00:50 +0200

    Changed shared_ptr to raw one.

--- src/agent/windows/WMICommunication.h
+++ src/agent/windows/WMICommunication.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <memory>
 #include <vector>
 #include <Wbemidl.h>
 
@@ -10,18 +9,15 @@ class WMICommunication
 public:
 	~WMICommunication();
 
-
 	bool WMIInit();
-
 	bool GetSMARTDataViaWMI();
 
-	std::shared_ptr<IWbemLocator> m_initialLocatorToWMI;
+	IWbemLocator* m_initialLocatorToWMI;
 	IWbemServices* m_services;
 	IEnumWbemClassObject* m_pEnumerator;
 	
 private:
 	std::vector<unsigned char> dataVector;	// In future here will be SMARTDATA structure
 	void FeedSmartDataStructure(BYTE* data, LONG& dataSize);
-	
-	
+		
 };
