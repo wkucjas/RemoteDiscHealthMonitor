65f73ee35a00b86b414bd3908fb4ede362395dcf
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-10-20 21:56:47 +0200

    Branch cleanup.

--- src/agent/WindowsSmartReader.cpp
+++
@@ -1,9 +0,0 @@
-#include "WindowsSmartReader.h"
-
-std::string WindowsSmartReader::ReadSMARTData()
-{
-	WMICommunication test;
-	bool a = test.WMIInit();
-	test.GetSMARTDataViaWMI();
-	return std::string("Windows S.M.A.R.T. DATA");
-}
