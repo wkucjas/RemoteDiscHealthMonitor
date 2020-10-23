f69856416e74a9f91757a578331c897f31893ffb
Michał Walenciak <Kicer86@gmail.com>
2020-10-23 21:47:16 +0200

    wire things up

--- src/monitor/main.cpp
+++ src/monitor/main.cpp
@@ -1,7 +1,9 @@
 
 #include <QGuiApplication>
+#include <QQuickItem>
 #include <QQuickView>
 
+#include "ActiveAgentsList.hpp"
 #include "AgentsExplorer.hpp"
 
 
@@ -9,12 +11,21 @@ int main(int argc, char** argv)
 {
     QGuiApplication app(argc, argv);
 
+    ActiveAgentsList activeAgents;
+
     AgentsExplorer agentsEnumerator;
+    QObject::connect(&agentsEnumerator, &AgentsExplorer::agentDiscovered, &activeAgents, &ActiveAgentsList::addAgent);
+    QObject::connect(&agentsEnumerator, &AgentsExplorer::agentLost, &activeAgents, &ActiveAgentsList::removeAgent);
+
     agentsEnumerator.startListening();
 
     QQuickView mainWindow(QUrl("qrc:/ui/MainWindow.qml"));
     mainWindow.setResizeMode(QQuickView::SizeRootObjectToView);
     mainWindow.show();
+
+    auto rootObject = mainWindow.rootObject();
+    auto view = rootObject->findChild<QObject*>("activeAgents");
+    view->setProperty("model", QVariant::fromValue(&activeAgents));
 
     return app.exec();
 }
