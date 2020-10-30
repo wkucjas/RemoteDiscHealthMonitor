8641f85456ea07cbd504b7aec5dda047b54f5790
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-10-30 20:26:41 +0100

    Add GeneralHealth class

---
+++ src/common/GeneralHealth.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdint.h>
+
+class GeneralHealth
+{
+public:
+    enum Health : uint8_t
+    {
+        GOOD,
+        BAD,
+        CHECK_STATUS
+    };
+
+    uint8_t GetStatus()
+    {
+        return m_status;
+    }
+
+    void SetStatus(Health _health)
+    {
+        m_status = _health;
+    }
+
+private:
+    uint8_t m_status;
+};
