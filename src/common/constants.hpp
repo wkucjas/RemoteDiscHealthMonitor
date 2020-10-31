c6ebbea00bb421525d21b86a9fe5cbc36a1372bb
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-10-31 11:44:31 +0100

    Constants update

--- src/common/constants.hpp
+++ src/common/constants.hpp
@@ -1,5 +1,7 @@
-
 #pragma once
+
+#include "ProtocolVersion.h"
 
 const QString ZeroConfServiceName = "_RDHMonitor._tcp";
 const unsigned int RDHMPort = 1630;
+const ProtocolVersion VersionOfProtocol = ProtocolVersion::VER_1;
