7a21b15515e16e70aa06eda299e989ea27bed5b1
Michał Walenciak <Kicer86@gmail.com>
2020-11-19 17:29:02 +0100

    extracting code

--- src/monitor/main.cpp
+++ src/monitor/main.cpp
@@ -11,6 +11,32 @@
 #include "common/GeneralHealth.h"
 #include "Configuration.hpp"
 
+
+
+namespace
+{
+    void storeHardcodedAgents(Configuration& config, const ActiveAgentsList& agentsList)
+    {
+        const QVector<AgentInformation>& allAgents = agentsList.agents();
+        QVector<AgentInformation> hardcodedAgents;
+
+        std::copy_if(allAgents.begin(), allAgents.end(), std::back_inserter(hardcodedAgents), [](const AgentInformation& info){
+            return info.detectionSource() == AgentInformation::DetectionSource::Hardcoded;
+        });
+
+        config.storeAgents(hardcodedAgents);
+    }
+
+    void restoreHardcodedAgents(Configuration& config, ActiveAgentsList& agentsList)
+    {
+        QVector<AgentInformation> hardcodedAgents = config.readAgents();
+
+        for(const auto& agent: hardcodedAgents)
+            agentsList.addAgent(agent);
+    }
+}
+
+
 int main(int argc, char** argv)
 {
     QGuiApplication app(argc, argv);
@@ -42,19 +68,11 @@ int main(int argc, char** argv)
     QObject::connect(rootObject, SIGNAL(newAgentRequested(QString, QString, QString)),
                      &manualAgentsValdiator, SLOT(addNewAgent(const QString &, const QString &, const QString &)));
 
-    QVector<AgentInformation> hardcodedAgents = config.readAgents();
-    for(const auto& agent: hardcodedAgents)
-        manualAgentsValdiator.addNewAgent(agent.name(), agent.host(), QString::number(agent.port()));
+    restoreHardcodedAgents(config, activeAgents);
 
     const int exitCode = app.exec();
 
-    const QVector<AgentInformation>& allAgents = activeAgents.agents();
-    hardcodedAgents.clear();
-    std::copy_if(allAgents.begin(), allAgents.end(), std::back_inserter(hardcodedAgents), [](const AgentInformation& info){
-        return info.detectionSource() == AgentInformation::DetectionSource::Hardcoded;
-    });
-
-    config.storeAgents(hardcodedAgents);
+    storeHardcodedAgents(config, activeAgents);
 
     return exitCode;
 }
