d67dc30a4da4e6a2839e218a6826ffe5ff369201
Michał Walenciak <Kicer86@gmail.com>
2020-11-27 20:07:03 +0100

    registering enum

--- src/monitor/main.cpp
+++ src/monitor/main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char** argv)
     Configuration config;
 
     qmlRegisterType<GeneralHealth>("RDHM", 1, 0, "HealthEnum");
+    qmlRegisterUncreatableType<AgentInformation>("RDHM", 1, 0, "AgentInformation", "Access to enum");
 
     AgentsStatusProvider statusProvider;
     AgentsList activeAgents(statusProvider);
