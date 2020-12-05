8c934e7fe3e7e03eeb34998ba9af22d9890bc051
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-12-05 18:18:01 +0100

    AgentTests fix on macos.

--- tests/agent/DiscStatusCalculatorTests.cpp
+++ tests/agent/DiscStatusCalculatorTests.cpp
@@ -2,13 +2,25 @@
 
 #include "IProbe.h"
 #include <memory>
+#if(_WIN32)
 #include "windows/WinGeneralAnalyzer.h"
+#else
+#include "linux/LinGeneralAnalyzer.h"
+#endif
 #include "DiscStatusCalculator.h"
 
 TEST(DiscStatusCalculatorTest, CalculateStatusWithGeneralProbe)
 {
     std::vector< DiscStatusCalculator::ProbePtr> probes;
+
+#if(_WIN32)
+    
     probes.emplace_back(std::make_unique<WinGeneralAnalyzer>());
+    
+#else
+    
+    probes.emplace_back(std::make_unique<LinGeneralAnalyzer>());
+#endif
     
     DiscStatusCalculator calc(probes);
 
