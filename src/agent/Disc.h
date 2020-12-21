84b29b892b0d8d8bab771286cc109e90d1b3e175
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2020-12-21 20:26:38 +0100

    Changed size field type to long long

--- src/agent/Disc.h
+++ src/agent/Disc.h
@@ -6,12 +6,12 @@ class Disc
 {
 public:
     Disc();
-    Disc(const std::string& _caption, const std::string& _deviceId, const std::string& _model,  const int& _partitions, const int& _size);
+    Disc(const std::string& _caption, const std::string& _deviceId, const std::string& _model,  const int& _partitions, const long long& _size);
 
 private:
     std::string caption;
     std::string deviceId;
     std::string model;
     int partitions;
-    int size;
+    long long size;
 };
