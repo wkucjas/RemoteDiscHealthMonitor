4f5c0a6bf40c756f2dfd6a9624bc0c90e65c7b79
Michał Walenciak <Kicer86@gmail.com>
2020-12-31 18:02:05 +0100

    returning full list of disks

--- src/agent/linux/LinuxDiskCollector.h
+++ src/agent/linux/LinuxDiskCollector.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <QString>
+
 #include "../IDiskCollector.h"
 
 
@@ -8,4 +10,7 @@ class LinuxDiskCollector: public IDiskCollector
 {
 public:
     std::vector<Disk> GetDisksList() override;
+
+private:
+    QString modelFor(const QString& device) const;
 };
