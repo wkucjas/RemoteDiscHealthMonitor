61c453397c573af84f93b0277c7936cf574e56f4
Michał Walenciak <Kicer86@gmail.com>
2020-12-31 16:21:51 +0100

    use LsblkOutputParser to parse lsblk output

--- src/agent/linux/LinuxDiskCollector.cpp
+++ src/agent/linux/LinuxDiskCollector.cpp
@@ -1,8 +1,22 @@
 
+#include <QDebug>
+#include <QProcess>
+
 #include "LinuxDiskCollector.h"
+#include "LsblkOutputParser.h"
 
 
 std::vector<Disk> LinuxDiskCollector::GetDisksList()
 {
+    QProcess lsblk;
+
+    lsblk.start("lsblk", { "-rMb" }, QProcess::ReadOnly);    // raw, merged arrays, size in bytes
+    lsblk.waitForFinished(5000);
+    const QByteArray output = lsblk.readAll();
+
+    qDebug() << output;
+
+    LsblkOutputParser::parse(output);
+
     return {};
 }
