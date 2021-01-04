725421a839af93f84d8c5cc5aadb5c975b5f440b
Michał Walenciak <Kicer86@gmail.com>
2021-01-04 20:40:56 +0100

    do not use <=> operator from c++20 as it is not available on all github action platforms

--- src/agent/linux/LsblkOutputParser.h
+++ src/agent/linux/LsblkOutputParser.h
@@ -15,7 +15,11 @@ public:
         int major;
         int minor;
 
-        friend auto operator<=>(const LsblkEntry &, const LsblkEntry &) = default;
+        friend auto operator==(const LsblkEntry& lhs, const LsblkEntry& rhs)
+        {
+            return std::tie(lhs.name, lhs.type, lhs.size, lhs.partitions, lhs.major, lhs.minor) ==
+                   std::tie(rhs.name, rhs.type, rhs.size, rhs.partitions, rhs.major, rhs.minor);
+        }
     };
 
     static std::vector<LsblkEntry> parse(const QByteArray& output);
