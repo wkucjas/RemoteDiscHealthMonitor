19ecc234fe92a668951b70e1a2413010eaafeb65
Michał Walenciak <Kicer86@gmail.com>
2021-01-13 21:26:54 +0100

    adding missing destructor

--- src/agent/linux/IPartitionsManager.h
+++ src/agent/linux/IPartitionsManager.h
@@ -3,6 +3,9 @@
 
 class IPartitionsManager
 {
+public:
+    virtual ~IPartitionsManager() = default;
+
     virtual bool isPartition(const QString& deviceName) = 0;
     virtual QString diskForPartition(const QString& deviceName) = 0;
 };
