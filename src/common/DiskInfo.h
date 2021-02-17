1aa8ae29a1dc469368beb00a9f4f33fe881abe9d
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2021-02-17 20:56:34 +0100

    Server extended by adding information about disks to message.

--- src/common/DiskInfo.h
+++ src/common/DiskInfo.h
@@ -12,6 +12,32 @@ public:
 	{
 	}
 
+	DiskInfo(std::string _name, GeneralHealth::Health _health)
+		: m_name(_name), m_health(_health)
+	{
+	}
+
+	void SetName(const std::string& _name)
+	{
+		m_name = _name;
+	}
+
+	std::string GetName()
+	{
+		return m_name;
+	};
+
+	void SetHealth(const GeneralHealth::Health& _health)
+	{
+		m_health = _health;
+	}
+
+	GeneralHealth::Health GetHealth()
+	{
+		return m_health;
+	}
+
+private:
 	std::string m_name;
 	GeneralHealth::Health m_health;
 	SmartData m_smart;
