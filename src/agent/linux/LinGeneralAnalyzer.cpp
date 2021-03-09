b7f0e7416102d58fb317094c4e0e0fb9ad439705
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2021-03-09 23:34:15 +0100

    Linux build fix - LinuxGeneralAnalyzer.h - Adjust method return value. 3

--- src/agent/linux/LinGeneralAnalyzer.cpp
+++ src/agent/linux/LinGeneralAnalyzer.cpp
@@ -22,7 +22,7 @@ GeneralHealth::Health LinGeneralAnalyzer::GetStatus(const Disk& disk)
 }
 
 
-RawData LinGeneralAnalyzer::GetRawData(const Disk& disk)
+IProbe::RawData LinGeneralAnalyzer::GetRawData(const Disk& disk)
 {
     std::string result;
 
--- src/agent/linux/LinGeneralAnalyzer.h
+++ src/agent/linux/LinGeneralAnalyzer.h
@@ -16,7 +16,7 @@ public:
     LinGeneralAnalyzer(std::shared_ptr<IPartitionsManager>);
 
     GeneralHealth::Health GetStatus(const Disk& disk) override;
-    RawData GetRawData(const Disk& disk) override;
+    IProbe::RawData GetRawData(const Disk& disk) override;
 
 private:
     std::map<Disk, std::set<QString>> m_errors;
