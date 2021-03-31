d59a018fe4073e15363bf1729800860864429e94
Michał Walenciak <Kicer86@gmail.com>
2021-03-31 21:01:34 +0200

    grouping setters/getters

--- src/common/DiskInfo.h
+++ src/common/DiskInfo.h
@@ -9,28 +9,22 @@
 class DiskInfo
 {
 public:
-
 	DiskInfo();
 
 	DiskInfo(std::string _name, GeneralHealth::Health& _health);
 
+	void SetHealth(const GeneralHealth::Health& _health);
 	void SetName(const std::string& _name);
 
 	std::string GetName() const;
-
-	void SetHealth(const GeneralHealth::Health& _health);
-
 	GeneralHealth::Health GetHealth() const;
 
-
 	bool operator== (const DiskInfo& _other) const;
-
 	bool operator<(const DiskInfo&)const;
 
 private:
 	std::string m_name;
 	GeneralHealth::Health m_health;
-
 };
 
 
